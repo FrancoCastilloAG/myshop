73e4454a861d8e9397b8fe583106ad3db9f8d227
fcastillo <fcastillo10@carnege.science.edu>
2025-10-06 20:12:07 -0300

    fix webhook importation no critical

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,24 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { initializeApp, getApps } from "firebase/app";
 import { getDatabase, ref, push, set } from "firebase/database";
 import { sendOrderEmails } from "./emailUtils";
+import { app } from "../../../firebaseconfig";
 
-// Config Firebase
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-if (!getApps().length) {
-  initializeApp(firebaseConfig);
-}
-
-const db = getDatabase();
+// Usar la instancia de app ya inicializada
+const db = getDatabase(app);
 
 export async function POST(req: NextRequest) {
   try {
