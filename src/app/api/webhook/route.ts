32d067b61cde5f83957008a79f0dac46209cd70d
fcastillo <fcastillo10@carnege.science.edu>
2025-10-08 03:16:50 -0300

    fix import

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,23 +1,11 @@
-
 import { NextRequest, NextResponse } from "next/server";
 import { getDatabase, ref, push, set } from "firebase/database";
-
-// Config Firebase
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-if (!getApps().length) {
-  initializeApp(firebaseConfig);
-}
+import { app } from "../../../firebaseconfig";
+import { sendOrderEmails } from "../email/route";
 
 const db = getDatabase(app);
+
+
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
